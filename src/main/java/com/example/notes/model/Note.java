1772d1dbc6364a204b1ad2d526c877a3ec29e6d0
Archana <archanadby05@gmail.com>
2025-12-20 23:53:29 +0530

    Convert Note model to JPA entity

--- src/main/java/com/example/notes/model/Note.java
+++ src/main/java/com/example/notes/model/Note.java
@@ -1,7 +1,15 @@
 package com.example.notes.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String title;
     private String content;
 
@@ -18,13 +26,11 @@ public class Note {
     public Long getId(){
         return id;
     }
-    public void setId(Long id){
-        this.id = id;
-    }
 
     public String getTitle(){
         return title;
     }
+
     public void setTitle(String title){
         this.title = title;
     }
@@ -32,6 +38,7 @@ public class Note {
     public String getContent(){
         return content;
     }
+    
     public void setContent(String content){
         this.content = content;
     }
